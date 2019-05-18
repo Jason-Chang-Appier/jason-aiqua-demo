87f1d33834d533a6b4e542691255e9a11d81babd
張天佑 <zhangtianyou@TienYuChangMacBookAir.local>
2019-05-18 22:54:48 +0800

    test

--- my-sw.js
+++ my-sw.js
@@ -6,9 +6,8 @@ self.addEventListener('install', function(event) {
 
 
 self.addEventListener('fetch', function(event) {
-  if (event.request.url.indexOf("https://aiqua-demo.herokuapp.com/") >= 0) {
-    console.log('v3 SW fetch:', event.request.url);
-  }
+  console.log('v3 SW fetch:', event.request.url);
+  
   //event.respondWith(new Response("request came from s2 v1"));
 });
 
