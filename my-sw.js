de359bd344be17e359978a4f441b2c20a2ef7cfe
張天佑 <zhangtianyou@TienYuChangMacBookAir.local>
2019-05-18 22:04:30 +0800

    test

--- my-sw.js
+++ my-sw.js
@@ -1,8 +1,12 @@
 self.addEventListener('install', function(event) {
-  console.log("try to install v1 service worker!")
+  event.waitUntil(
+    console.log("try to install v1 service worker!")
+  );
 });
 
 
 self.addEventListener('activate', function(event) {
-  console.log("try to activate v1 service worker!")
+  event.waitUntil(
+    console.log("try to activate v1 service worker!")
+  );
 });
