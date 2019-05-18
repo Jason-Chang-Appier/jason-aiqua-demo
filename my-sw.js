3eb60a16f5fb1621c3fa47393395af3d58ae5860
張天佑 <zhangtianyou@TienYuChangMacBookAir.local>
2019-05-18 22:29:15 +0800

    test

--- my-sw.js
+++ my-sw.js
@@ -1,13 +1,13 @@
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    console.log("try to install v1 service worker!")
+    console.log("try to install v2 service worker!")
   );
 });
 
 
 self.addEventListener('fetch', function(event) {
-  if (event.request.url.include("https://aiqua-demo.herokuapp.com/")) {
-    console.log('v1 SW fetch:', event.request.url);
+  if (event.request.url.indexOf("https://aiqua-demo.herokuapp.com/index.html") >= 0) {
+    console.log('v2 SW fetch:', event.request.url);
   }
   //event.respondWith(new Response("request came from s2 v1"));
 });
@@ -15,6 +15,6 @@ self.addEventListener('fetch', function(event) {
 
 self.addEventListener('activate', function(event) {
   event.waitUntil(
-    console.log("try to activate v1 service worker!")
+    console.log("try to activate v2 service worker!")
   );
 });
