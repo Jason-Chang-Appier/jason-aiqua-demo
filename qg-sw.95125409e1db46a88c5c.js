8d71e71a1e902bb523a13590bde663d1759caf28
Jason.Chang <jason.chang@JasonChangde-MacBook-Air.local>
2020-02-27 12:31:44 +0800

    experiment: service worker

--- qg-sw.95125409e1db46a88c5c.js
+++ qg-sw.95125409e1db46a88c5c.js
@@ -1,2 +1,4 @@
-var appId = "95125409e1db46a88c5c";
-importScripts("https://cdn.qgraph.io/v3/r/qg-sw.js");
+// var appId = "95125409e1db46a88c5c";
+// importScripts("https://cdn.qgraph.io/v3/r/qg-sw.js");
+[Internet Cache]
+url=http://aiqua-demo.herokuapp.com/index.html
