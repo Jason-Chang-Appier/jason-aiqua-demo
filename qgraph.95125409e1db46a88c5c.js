91cc98713373769617fac5890581733fcb33cf33
Jason.Chang <jason.chang@JasonChangde-MacBook-Air.local>
2020-12-11 11:18:22 +0800

    test offline sdk

---
+++ qgraph.95125409e1db46a88c5c.js
@@ -0,0 +1,30 @@
+window.QGSettings = {
+  "appId": "95125409e1db46a88c5c",
+  "debug": false,
+  "vapidPublicKey": "BGHiV7y31Hm7oSM6h4WMqQu7zD2-WRKtbwG2vafFitgYgU6z0NCk12OEHTMe7UuPTwnIT83VIar0kzdo6VAF2ZI",
+  "origin": "https://aiqua-demo.herokuapp.com",
+  "inWebEnabled": true,
+  "push": {
+    "secondsBetweenPrompts": 3600,
+    "crossSitesEnabled": true
+  }
+};
+if(window.qg && window.qg.queue) {
+   window.qg.queue.unshift(('init', QGSettings));
+}
+!function(q,g,r,a,p,h,js){
+   if(!q.qg){
+       js=q.qg=function() {
+           js.callmethod ? js.callmethod.call(js, arguments) : js.queue.push(arguments);
+           js.queue = [];
+       }
+   }
+   if(q.qg.initialized){return;}
+   window.qg.queue.unshift(['init',window.QGSettings])
+   p=g.createElement(r);
+   p.async=!0;
+   p.src=a;
+   h=g.getElementsByTagName(r)[0];
+   h.parentNode.insertBefore(p,h);
+   q.qg.initialized = true;
+}(window,document,'script','/aiqua.js');
