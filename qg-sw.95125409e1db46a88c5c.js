11a818eede5cabd41d09d0c711f4a8ba700e202f
jason <tienyu.chang@appier.com>
2023-01-11 15:22:04 +0800

    change service worker import way

--- qg-sw.95125409e1db46a88c5c.js
+++ qg-sw.95125409e1db46a88c5c.js
@@ -1,7 +1,7 @@
 var appId = "95125409e1db46a88c5c";
-// importScripts("https://cdn.qgraph.io/v3/r/qg-sw.js");
+importScripts("https://cdn.qgraph.io/v3/r/qg-sw.js");
 
-var worker = new Worker("https://cdn.qgraph.io/v3/r/qg-sw.js");
-worker.postMessage();
+// var worker = new Worker("https://cdn.qgraph.io/v3/r/qg-sw.js");
+// worker.postMessage();
 // [Internet Cache]
 // url=http://aiqua-demo.herokuapp.com/index.html
